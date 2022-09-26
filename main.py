d81491f94e6183153b0e6cf99b8ceaa7be996c98
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-09-26 10:16:36 -0600

    started menu

--- main.py
+++ main.py
@@ -3,7 +3,17 @@
 ### SUBROUTINES ### 
 
 # Inputs # 
-
+def menu():
+    """
+    display different scenarios and asks user to make a selection
+    :return: int
+    """
+    print("Welcome to the Official Navy Cannon Distance Calculator! Find the total distance the cannonball travels from the cannon! ")
+    print("""1. Horizontal to the water 
+2. Parabolic to a level boat 
+3. Parabolic to a smaller boat far away""")
+    SCENARIONUMBER = input("Enter the scenario number: ")
+    
 # Processing # 
 
 # Outputs # 
