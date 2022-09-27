327cec69051508a87fcec4fc7716b31a2fae150f
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-09-26 21:58:10 -0600

    added calculations and fixed inputs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 
-import math 
+import math
+from tkinter import VERTICAL 
 
 ### SUBROUTINES ### 
 
@@ -25,9 +26,11 @@ def menu():
     :return: int
     """
     print("Welcome to the Official Navy Cannon Distance Calculator! Find the total distance the cannonball travels from the cannon! ")
-    print("""1. Horizontal to the water 
+    print("""
+1. Horizontal to the water 
 2. Parabolic to a level boat 
-3. Parabolic to a smaller boat far away""")
+3. Parabolic to a smaller boat far away
+""")
     SCENARIONUMBER = input("What is the scenario number? ")
     SCENARIONUMBER = checkInt(SCENARIONUMBER)
     return SCENARIONUMBER
@@ -52,17 +55,26 @@ def getAngle():
 
 def getHeight(): 
     """
-    asks user to input the initial height of the cannonball above the horizon in meters
+    asks user to input the initial height of the cannonball above the water in meters
     :return: int
     """
-    HEIGHT = input("What is the initial height of the cannonball above the horizon? (m): ")
+    HEIGHT = input("What is the height of the cannonball above the water? (m): ")
     HEIGHT = checkInt(HEIGHT)
     return HEIGHT
-    
+
+def getHeight3():
+    """
+    asks the user to input the inital height of the cannonball above the enenmy ship in meters
+    :return: int 
+    """
+    HEIGHT = input("What is the initial height of the cannonball above the enemy ship? (m): ")
+    HEIGHT = checkInt(HEIGHT)
+    return HEIGHT 
+
 # Processing # 
 def calculateTime1(HEIGHT): 
     """
-    calculate time in scenario 1 when given height 
+    calculate time in scenario 1 when given height (m)
     :param HEIGHT: int
     :return: int 
     """
@@ -71,13 +83,52 @@ def calculateTime1(HEIGHT):
 
 def calculateHorizontalSpeed(SPEED, ANGLE):
     """
-    calculate horizontal speed when given cannonball speed and angle 
+    calculate horizontal speed when given cannonball speed (m/s) and angle (degrees)
     :param SPEED: int
     :param ANGLE: int
     :return: int
     """
     HORIZONTALSPEED = SPEED * math.cos(ANGLE)
     return HORIZONTALSPEED
+
+def calculateVerticalSpeed(SPEED, ANGLE):
+    """
+    calculate vertical speed when given cannonball speed (m/s) and angle (degrees)
+    :param SPEED: int 
+    :param ANGLE: int 
+    :return: int 
+    """
+    VERTICALSPEED = SPEED * math.sin(ANGLE)
+    return VERTICALSPEED
+
+def calculateTotalTime(VERTICALSPEED):
+    """
+    calculate the total time when given vertical speed in m/s
+    :param VERTICALSPEED: int
+    :return: int
+    """
+    TIME = VERTICALSPEED / 9.81
+    TOTALTIME = TIME * 2
+    return TOTALTIME
+
+def calculateDistance(HORIZONTALSPEED, TOTALTIME):
+    """
+    calculate distance from horizontal speed (m/s) and total time (s)
+    :param HORIZONTALSPEED: int
+    :param TOTALTIME: int
+    :return: int
+    """
+    DISTANCE = HORIZONTALSPEED * TOTALTIME
+    return DISTANCE 
+
+def calculateDistance3(VERTICALSPEED, HEIGHT):
+    """
+    calculate total distance from vertical speed and height above ship
+    :param VERTICALSPEED: int
+    :param HEIGHT: int
+    :return: int
+    """
+    DISTANCEPEAK = VERTICALSPEED ** 2 / (2 * 9.81)
 
 # Outputs # 
 
